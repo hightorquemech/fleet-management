d4fbbdc5293fcfe3c8328a8f72a7a7d18266d214
Cam Remesz <saltyndead@gmail.com>
2024-10-19 20:58:27 -0400

    Start JS

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,59 @@
-
-const next = document.querySelector("button");
-
-
-
-
-
-
+const print = document.querySelector("button");
+const otherGuyInputs = Array.from(document.querySelectorAll('input')).filter((input) => input.id.includes('og') && !input.id.includes('total'));
+const otherGuyTotal = document.querySelector('#og-yearly-total') as HTMLInputElement;
+const withUsInputs = Array.from(document.querySelectorAll('input')).filter((input) => input.id.includes('wu') && !input.id.includes('total'));
+const withUsTotal = document.querySelector('#wu-yearly-total') as HTMLInputElement;
+const totalSavings = document.querySelector('#total-savings') as HTMLInputElement;
 
 
 
+function checkOtherGuyTotal() {
+  const canCalcTotal = otherGuyInputs.every(input => {
+    return !!input.value
+  })
+
+  if (canCalcTotal) {
+    const values = otherGuyInputs.map(input => Number(Number(input.value).toFixed(2)));
+    const total = (values[0] + values[1] + values[2]) * values[3] * values[4];
+    otherGuyTotal.value = `${total}`;
+  }
+
+  if(otherGuyTotal.value && withUsTotal.value) {
+    totalSavings.value = (Number(otherGuyTotal.value) - Number(withUsTotal.value)).toFixed()
+  }
+}
+
+function checkWithUsTotal() {
+  const canCalcTotal = withUsInputs.every(input => {
+    return !!input.value
+  })
+
+  if (canCalcTotal) {
+    const values = withUsInputs.map(input => Number(Number(input.value).toFixed(2)));
+    const total = (values[0] + values[1] + values[2]) * values[3] * values[4];
+    withUsTotal.value = `${total}`;
+  }
+
+  if(otherGuyTotal.value && withUsTotal.value) {
+    totalSavings.value = (Number(otherGuyTotal.value) - Number(withUsTotal.value)).toFixed()
+  }
+}
+
+
+console.log(otherGuyInputs);
 
 
 
 
-next?.addEventListener('click', () => {
+
+
+
+
+
+
+otherGuyInputs.forEach(input => input.addEventListener('input', checkOtherGuyTotal));
+withUsInputs.forEach(input => input.addEventListener('input', checkWithUsTotal));
+
+print?.addEventListener('click', () => {
   window.print();
 })
